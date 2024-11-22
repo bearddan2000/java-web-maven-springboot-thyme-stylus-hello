4534e6cd89e7c4c690b8b6ef4b4fd5b3d831157a
bearddan2000 <bearddan2000@yahoo.com>
2024-11-22 09:39:27 -0500

    initial commit

--- bin/src/main/java/example/Main.java
+++ bin/src/main/java/example/Main.java
@@ -1,26 +1,13 @@
 package example;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
 // Main class
-// Implementing CommandLineRunner interface
-public class Main implements CommandLineRunner 
+public class Main
 {
-
-    // Method 1
-    // run() method for springBootApplication to execute
-    @Override
-    public void run(String[] args) throws Exception 
-    {
-
-        // Print statement when method is called
-        System.out.println("HEllo world");
-    }
-
     // Method 2
     // Main driver method
     public static void main(String[] args) 
